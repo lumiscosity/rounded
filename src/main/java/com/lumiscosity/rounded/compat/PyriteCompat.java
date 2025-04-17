166d95113db0eeaecf6fc515c7db71ed85baeeb5
Cassian Godsted <cassian@godsted.com>
2025-04-17 14:53:09 -0400

    Pyrite compatibility

---
+++ src/main/java/com/lumiscosity/rounded/compat/PyriteCompat.java
@@ -0,0 +1,196 @@
+package com.lumiscosity.rounded.compat;
+
+            import net.minecraft.block.AbstractBlock;
+            import net.minecraft.block.Block;
+            import net.minecraft.item.BlockItem;
+            import net.minecraft.item.Item;
+            import net.minecraft.registry.Registries;
+            import net.minecraft.util.Identifier;
+
+            import static com.lumiscosity.rounded.blocks.RegisterBlocks.register_treated_plank;
+
+            public class PyriteCompat {
+                public static final Block TREATED_WHITE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "white_stained_planks")))
+                );
+                public static final Item TREATED_WHITE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_WHITE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_LIGHT_GRAY_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "light_gray_stained_planks")))
+                );
+                public static final Item TREATED_LIGHT_GRAY_STAINED_PLANKS_ITEM = new BlockItem(TREATED_LIGHT_GRAY_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_GRAY_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "gray_stained_planks")))
+                );
+                public static final Item TREATED_GRAY_STAINED_PLANKS_ITEM = new BlockItem(TREATED_GRAY_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_BLACK_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "black_stained_planks")))
+                );
+                public static final Item TREATED_BLACK_STAINED_PLANKS_ITEM = new BlockItem(TREATED_BLACK_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_BROWN_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "brown_stained_planks")))
+                );
+                public static final Item TREATED_BROWN_STAINED_PLANKS_ITEM = new BlockItem(TREATED_BROWN_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_RED_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "red_stained_planks")))
+                );
+                public static final Item TREATED_RED_STAINED_PLANKS_ITEM = new BlockItem(TREATED_RED_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_ORANGE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "orange_stained_planks")))
+                );
+                public static final Item TREATED_ORANGE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_ORANGE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_HONEY_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "honey_stained_planks")))
+                );
+                public static final Item TREATED_HONEY_STAINED_PLANKS_ITEM = new BlockItem(TREATED_HONEY_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_YELLOW_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "yellow_stained_planks")))
+                );
+                public static final Item TREATED_YELLOW_STAINED_PLANKS_ITEM = new BlockItem(TREATED_YELLOW_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_POISONOUS_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "poisonous_stained_planks")))
+                );
+                public static final Item TREATED_POISONOUS_STAINED_PLANKS_ITEM = new BlockItem(TREATED_POISONOUS_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_LIME_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "lime_stained_planks")))
+                );
+                public static final Item TREATED_LIME_STAINED_PLANKS_ITEM = new BlockItem(TREATED_LIME_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_GREEN_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "green_stained_planks")))
+                );
+                public static final Item TREATED_GREEN_STAINED_PLANKS_ITEM = new BlockItem(TREATED_GREEN_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_GLOW_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "glow_stained_planks")))
+                );
+                public static final Item TREATED_GLOW_STAINED_PLANKS_ITEM = new BlockItem(TREATED_GLOW_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_CYAN_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "cyan_stained_planks")))
+                );
+                public static final Item TREATED_CYAN_STAINED_PLANKS_ITEM = new BlockItem(TREATED_CYAN_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_NOSTALGIA_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "nostalgia_stained_planks")))
+                );
+                public static final Item TREATED_NOSTALGIA_STAINED_PLANKS_ITEM = new BlockItem(TREATED_NOSTALGIA_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_STAR_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "star_stained_planks")))
+                );
+                public static final Item TREATED_STAR_STAINED_PLANKS_ITEM = new BlockItem(TREATED_STAR_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_LIGHT_BLUE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "light_blue_stained_planks")))
+                );
+                public static final Item TREATED_LIGHT_BLUE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_LIGHT_BLUE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_BLUE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "blue_stained_planks")))
+                );
+                public static final Item TREATED_BLUE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_BLUE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_PURPLE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "purple_stained_planks")))
+                );
+                public static final Item TREATED_PURPLE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_PURPLE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_DRAGON_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "dragon_stained_planks")))
+                );
+                public static final Item TREATED_DRAGON_STAINED_PLANKS_ITEM = new BlockItem(TREATED_DRAGON_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_MAGENTA_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "magenta_stained_planks")))
+                );
+                public static final Item TREATED_MAGENTA_STAINED_PLANKS_ITEM = new BlockItem(TREATED_MAGENTA_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_ROSE_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "rose_stained_planks")))
+                );
+                public static final Item TREATED_ROSE_STAINED_PLANKS_ITEM = new BlockItem(TREATED_ROSE_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_PINK_STAINED_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "pink_stained_planks")))
+                );
+                public static final Item TREATED_PINK_STAINED_PLANKS_ITEM = new BlockItem(TREATED_PINK_STAINED_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_RED_MUSHROOM_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "red_mushroom_planks")))
+                );
+                public static final Item TREATED_RED_MUSHROOM_PLANKS_ITEM = new BlockItem(TREATED_RED_MUSHROOM_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_BROWN_MUSHROOM_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "brown_mushroom_planks")))
+                );
+                public static final Item TREATED_BROWN_MUSHROOM_PLANKS_ITEM = new BlockItem(TREATED_BROWN_MUSHROOM_PLANKS, new Item.Settings());
+            
+            
+                public static final Block TREATED_AZALEA_PLANKS = new Block(
+                    Block.Settings.copy(Registries.BLOCK.get(Identifier.of("pyrite", "azalea_planks")))
+                );
+                public static final Item TREATED_AZALEA_PLANKS_ITEM = new BlockItem(TREATED_AZALEA_PLANKS, new Item.Settings());
+            
+               public static void register() {        register_treated_plank("compat/pyrite/treated_white_stained_planks", TREATED_WHITE_STAINED_PLANKS, TREATED_WHITE_STAINED_PLANKS_ITEM, "pyrite", "white_stained");
+        register_treated_plank("compat/pyrite/treated_light_gray_stained_planks", TREATED_LIGHT_GRAY_STAINED_PLANKS, TREATED_LIGHT_GRAY_STAINED_PLANKS_ITEM, "pyrite", "light_gray_stained");
+        register_treated_plank("compat/pyrite/treated_gray_stained_planks", TREATED_GRAY_STAINED_PLANKS, TREATED_GRAY_STAINED_PLANKS_ITEM, "pyrite", "gray_stained");
+        register_treated_plank("compat/pyrite/treated_black_stained_planks", TREATED_BLACK_STAINED_PLANKS, TREATED_BLACK_STAINED_PLANKS_ITEM, "pyrite", "black_stained");
+        register_treated_plank("compat/pyrite/treated_brown_stained_planks", TREATED_BROWN_STAINED_PLANKS, TREATED_BROWN_STAINED_PLANKS_ITEM, "pyrite", "brown_stained");
+        register_treated_plank("compat/pyrite/treated_red_stained_planks", TREATED_RED_STAINED_PLANKS, TREATED_RED_STAINED_PLANKS_ITEM, "pyrite", "red_stained");
+        register_treated_plank("compat/pyrite/treated_orange_stained_planks", TREATED_ORANGE_STAINED_PLANKS, TREATED_ORANGE_STAINED_PLANKS_ITEM, "pyrite", "orange_stained");
+        register_treated_plank("compat/pyrite/treated_honey_stained_planks", TREATED_HONEY_STAINED_PLANKS, TREATED_HONEY_STAINED_PLANKS_ITEM, "pyrite", "honey_stained");
+        register_treated_plank("compat/pyrite/treated_yellow_stained_planks", TREATED_YELLOW_STAINED_PLANKS, TREATED_YELLOW_STAINED_PLANKS_ITEM, "pyrite", "yellow_stained");
+        register_treated_plank("compat/pyrite/treated_poisonous_stained_planks", TREATED_POISONOUS_STAINED_PLANKS, TREATED_POISONOUS_STAINED_PLANKS_ITEM, "pyrite", "poisonous_stained");
+        register_treated_plank("compat/pyrite/treated_lime_stained_planks", TREATED_LIME_STAINED_PLANKS, TREATED_LIME_STAINED_PLANKS_ITEM, "pyrite", "lime_stained");
+        register_treated_plank("compat/pyrite/treated_green_stained_planks", TREATED_GREEN_STAINED_PLANKS, TREATED_GREEN_STAINED_PLANKS_ITEM, "pyrite", "green_stained");
+        register_treated_plank("compat/pyrite/treated_glow_stained_planks", TREATED_GLOW_STAINED_PLANKS, TREATED_GLOW_STAINED_PLANKS_ITEM, "pyrite", "glow_stained");
+        register_treated_plank("compat/pyrite/treated_cyan_stained_planks", TREATED_CYAN_STAINED_PLANKS, TREATED_CYAN_STAINED_PLANKS_ITEM, "pyrite", "cyan_stained");
+        register_treated_plank("compat/pyrite/treated_nostalgia_stained_planks", TREATED_NOSTALGIA_STAINED_PLANKS, TREATED_NOSTALGIA_STAINED_PLANKS_ITEM, "pyrite", "nostalgia_stained");
+        register_treated_plank("compat/pyrite/treated_star_stained_planks", TREATED_STAR_STAINED_PLANKS, TREATED_STAR_STAINED_PLANKS_ITEM, "pyrite", "star_stained");
+        register_treated_plank("compat/pyrite/treated_light_blue_stained_planks", TREATED_LIGHT_BLUE_STAINED_PLANKS, TREATED_LIGHT_BLUE_STAINED_PLANKS_ITEM, "pyrite", "light_blue_stained");
+        register_treated_plank("compat/pyrite/treated_blue_stained_planks", TREATED_BLUE_STAINED_PLANKS, TREATED_BLUE_STAINED_PLANKS_ITEM, "pyrite", "blue_stained");
+        register_treated_plank("compat/pyrite/treated_purple_stained_planks", TREATED_PURPLE_STAINED_PLANKS, TREATED_PURPLE_STAINED_PLANKS_ITEM, "pyrite", "purple_stained");
+        register_treated_plank("compat/pyrite/treated_dragon_stained_planks", TREATED_DRAGON_STAINED_PLANKS, TREATED_DRAGON_STAINED_PLANKS_ITEM, "pyrite", "dragon_stained");
+        register_treated_plank("compat/pyrite/treated_magenta_stained_planks", TREATED_MAGENTA_STAINED_PLANKS, TREATED_MAGENTA_STAINED_PLANKS_ITEM, "pyrite", "magenta_stained");
+        register_treated_plank("compat/pyrite/treated_rose_stained_planks", TREATED_ROSE_STAINED_PLANKS, TREATED_ROSE_STAINED_PLANKS_ITEM, "pyrite", "rose_stained");
+        register_treated_plank("compat/pyrite/treated_pink_stained_planks", TREATED_PINK_STAINED_PLANKS, TREATED_PINK_STAINED_PLANKS_ITEM, "pyrite", "pink_stained");
+        register_treated_plank("compat/pyrite/treated_red_mushroom_planks", TREATED_RED_MUSHROOM_PLANKS, TREATED_RED_MUSHROOM_PLANKS_ITEM, "pyrite", "red_mushroom");
+        register_treated_plank("compat/pyrite/treated_brown_mushroom_planks", TREATED_BROWN_MUSHROOM_PLANKS, TREATED_BROWN_MUSHROOM_PLANKS_ITEM, "pyrite", "brown_mushroom");
+        register_treated_plank("compat/pyrite/treated_azalea_planks", TREATED_AZALEA_PLANKS, TREATED_AZALEA_PLANKS_ITEM, "pyrite", "azalea");
+
+        }
+    }
